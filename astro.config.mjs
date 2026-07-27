aac8c52250bc923e3b62de4f823735ec3ba7720a
cwhpilar <cwh.pilar2026@gmail.com>
2026-07-27 12:15:44 +0100

    fix: corregir comentario sobre la configuración de la internacionalización en astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,11 +7,13 @@ import tailwindcss from "@tailwindcss/vite";
 // https://astro.build/config
 export default defineConfig({
   devToolbar: { enabled: false },
+
+  // añadimos los plugins de css tailwindcss para astro
   vite: {
     plugins: [tailwindcss()],
   },
 
-  //agregammos la internalización de idiomas
+  // agregammos la internalización de idiomas a la configuración
   i18n: {
     defaultLocale: 'es',
     locales: ['es', 'en'],
